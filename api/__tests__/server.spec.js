04521016d11d2f94ffa6f10204884088e4d59c70
Dajuan Mcdonald <16410995+FIOpwK@users.noreply.github.com>
2020-05-24 19:55:30 -0400

    removed comments from test file

--- api/__tests__/server.spec.js
+++ api/__tests__/server.spec.js
@@ -125,20 +125,3 @@ describe('integration testing', () => {
     })
     
 })
-//testing with async/await and es6 syntax
-// describe('testing', () => {
-// 	it('should return 200', async () => {
-// 	const response = await request(server).get('/')
-
-// 	expect(response.status).toBe(200)
-// 	})
-// })
-
-//testing with es5 syntax using return
-// describe('testing', function() {
-// 	it('should return 200', function() {
-// 		return request(server)
-// 			.get('/')
-// 			.expect(200)
-// 	})
-// })
