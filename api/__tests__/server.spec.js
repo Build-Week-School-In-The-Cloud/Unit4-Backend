dc8270f97e98e49a1fa983271b5a179d9ba7f4fb
Dajuan Mcdonald <16410995+FIOpwK@users.noreply.github.com>
2020-05-24 18:25:27 -0400

    first round draft tests

--- api/__tests__/server.spec.js
+++ api/__tests__/server.spec.js
@@ -1,3 +1,47 @@
+const request = require('supertest')
+const server = require('../server')
+const db = require('../../config/db-config')
+
+describe('integration testing', () => {
+    describe('GET /', () => {
+        test('should return 200 ok', async () => {
+            const response = await request(server).get('/')
+            expect(response.status).toBe(200)
+        })
+        test('should return JSON response ', async () => {
+            const response = await request(server).get('/')
+            expect(response.type).toMatch(/json/)
+        })
+
+
+    })
+
+    describe('GET /api', () => {
+        test('should return 404 status', async () => {
+                const response = await request(server).get('/api')
+                expect(response.status).toBe(404)
+        })
+        test('should return HTML response', async () => {
+            const response = await request(server).get('/api')
+                expect(response.type).toMatch(/html/)
+
+        })
+    })
+
+    describe('GET /api/admin', () => {
+        test('should return 200 ok', async () => {
+            const response = await request(server).get('/api/admin')
+            expect(response.status).toBe(200)
+        })
+        test('should return JSON response ', async () => {
+            const response = await request(server).get('/api/admin')
+            expect(response.type).toMatch(/json/)
+        })
+
+
+    })
+    
+})
 //testing with async/await and es6 syntax
 // describe('testing', () => {
 // 	it('should return 200', async () => {
